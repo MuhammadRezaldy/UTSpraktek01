5bf9924c2c46bedb6d348bef4ae49ab3e3c734e2
MuhammadRezaldy <rezaldymm1@gmail.com>
2022-12-15 14:09:52 +0800

    instance object

--- src/praktek01/MahasiswaAksi.java
+++ src/praktek01/MahasiswaAksi.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package praktek01;
 
-/**
- *
- * @author win 10
- */
 public class MahasiswaAksi {
-    
+    public static void main(String[] args) {
+        
+        Mahasiswa Harry = new Mahasiswa();
+    }
 }
